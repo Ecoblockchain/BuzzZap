0e522be4de0f0d3ede6fb8a0cdddb41ccc3310f3
Caspar Wylie <casparwylie@gmail.com>
2016-01-19 21:40:48 +0000

    g

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if(substr($_SERVER['PHP_SELF'], 0,3)=="/pr"){
 				header("Location: site_disabled.php");
 				exit();
 			}
-			if((isset($_GET['page']))&&(valid_page($_GET['page']))){
+			if((iset($_GET['page']))&&(valid_page($_GET['page']))){
 				if(get_static_content("last_errorf_size")<filesize("php-error.log")){
 					send_admin_note("There are new errors in the error log file");
 				}
