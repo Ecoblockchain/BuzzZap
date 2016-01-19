dc146d0faac56ffe3aba31c983001caccdd54c8c
Caspar Wylie <casparwylie@gmail.com>
2016-01-19 22:04:33 +0000

    g

--- admin_panel.php
+++ admin_panel.php
@@ -498,6 +498,7 @@ if(loggedin_as_admin()){
 				if(isset($_GET['rerr'])){
 					$handle = fopen("php-error.log", "w+");
 					fwrite($handle , ' ');
+					header("Location: index.php?page=home&sp=10");
 				}
 			?>
 		
