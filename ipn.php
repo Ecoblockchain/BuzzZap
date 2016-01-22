b9b2df7a27e7280785d530199d0d24b93fa1ff82
Caspar Wylie <casparwylie@gmail.com>
2016-01-22 17:33:56 +0000

    clean version5

--- ipn.php
+++ ipn.php
@@ -1,6 +1,5 @@
 <?php
 require("requires.php");
-$snc_suc_email = get_static_content("snc_suc_email");
 $raw_post_data = file_get_contents('php://input');
 $raw_post_array = explode('&', $raw_post_data);
 $myPost = array();
@@ -49,19 +48,8 @@ curl_close($ch);
 if (strcmp ($res, "VERIFIED") == 0) {
   
   $com_ident = htmlentities($_POST['custom']);
-  $myfile = fopen("ipnnnn.txt", "w");
-  fwrite($myfile, $com_ident);
-  fclose($myfile);
   $db->query("UPDATE com_act SET act = 1 WHERE act = 0 AND ipn = ".$db->quote($com_ident));
-  $com_id = $db->query("SELECT com_id FROM com_act WHERE ipn = ".$db->quote($com_ident)." LIMIT 1")->fetchColumn();
-  $leadername = $db->query("SELECT user_firstname FROM users WHERE user_com = ".$db->quote($com_id)." AND user_rank = 3 LIMIT 1");
-  $email = $db->query("SELECT user_email FROM users WHERE user_com = ".$db->quote($com_id)." AND user_rank = 3 LIMIT 1");;
-  $headers  = 'MIME-Version: 1.0' . "\r\n";
-  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-  $headers .= "From: Administration@buzzzap.com" . "\r\n";
-  $com_name = $db->query("SELECT com_name FROM communities WHERE com_id = ".$db->quote($com_id))->fetchColumn();
-  $body = static_cont_rec_vars($snc_suc_email);
-  mail($email,"BuzzZap Community Activation",$body,$headers);
-   
+
+  setcookie("snc-made-suc", "cid:"$db->query("SELECT com_id FROM com_act WHERE ipn = ".$db->quote($com_ident), time()+200));
 }
 ?>
