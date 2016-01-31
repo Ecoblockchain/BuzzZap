a7f2084c7cbd060a1951214079995a63d5d68634
Caspar Wylie <casparwylie@gmail.com>
2016-01-31 21:11:24 +0000

    STABLE - hidden coms system - so far for wof only

--- pages/wof.php
+++ pages/wof.php
@@ -9,17 +9,20 @@ if(loggedin()){
 		<span class = 'wof-cont-title'>BuzzZap Community Leader Board</span><br>
 		<br>
 		<?php
-			$get1 = $db->query("SELECT com_name FROM com_profile ORDER BY com_rep DESC LIMIT 20");
+			$hidden_coms = explode(",",get_static_content("hide_coms"));
+			$get1 = $db->query("SELECT com_name,com_id FROM com_profile ORDER BY com_rep DESC LIMIT 20");
 			$count = 1;
 			$color = "#59c3d8";
 			foreach($get1 as $com){
-				if($count==1){
-					$color = "gold";
-				}else if($count==2){
-					$color = "#59c3d8";
+				if(!in_array($com['com_id'], $hidden_coms)){
+					if($count==1){
+						$color = "gold";
+					}else if($count==2){
+						$color = "#59c3d8";
+					}
+					echo "<span style = 'color:".$color."'>".$count.".".add_profile_link($com['com_name'], 1, 'color: '.$color)."</span><hr size = '1'>";
+					$count++;
 				}
-				echo "<span style = 'color:".$color."'>".$count.".".add_profile_link($com['com_name'], 1, 'color: '.$color)."</span><hr size = '1'>";
-				$count++;
 			}
 		?>
 	</div>
@@ -27,17 +30,19 @@ if(loggedin()){
 		<span class = 'wof-cont-title'>BuzzZap User Leader Board</span><br>
 		<br>
 		<?php
-			$get2 = $db->query("SELECT user_username FROM users ORDER BY user_rep DESC LIMIT 20");
+			$get2 = $db->query("SELECT user_username,user_com FROM users ORDER BY user_rep DESC LIMIT 20");
 			$count = 1;
 			$color = "#59c3d8";
 			foreach($get2 as $user){
-				if($count==1){
-					$color = "gold";
-				}else if($count==2){
-					$color = "#59c3d8";
+				if(!in_array($user['user_com'], $hidden_coms)){
+					if($count==1){
+						$color = "gold";
+					}else if($count==2){
+						$color = "#59c3d8";
+					}
+					echo "<span style = 'color:".$color."'>".$count.".".add_profile_link($user['user_username'], 0, 'color: '.$color)."</span><hr size = '1'>";
+					$count++;
 				}
-				echo "<span style = 'color:".$color."'>".$count.".".add_profile_link($user['user_username'], 0, 'color: '.$color)."</span><hr size = '1'>";
-				$count++;
 			}
 		?>
 	</div>
