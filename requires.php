42d77a1a362ca630929dc351ec0e4c880d3812ab
Caspar Wylie <casparwylie@gmail.com>
2016-02-08 17:04:53 +0000

    mail sys

--- requires.php
+++ requires.php
@@ -17,7 +17,7 @@
 <?php
 require("functions.php"); 
 require("connect_db.php");
-require('class.phpmailer.php');
+require("PHPMailer/PHPMailerAutoload.php");
 if(isset($_GET['page'])){
 	require("ext/predtextjs.php");
 }
