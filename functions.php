320c5dccb84aaa53aaacb224a4f9ab19acf8a87b
Caspar Wylie <casparwylie@gmail.com>
2016-02-08 19:59:31 +0000

    t

--- functions.php
+++ functions.php
@@ -1934,8 +1934,7 @@ function get_group_rep($gid){
 	return $rep;
 }
 function send_mail($to,$subject,$body,$from){
-	global $mail;
-	
+
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 	$headers .= "From: ".$from. "\r\n";
