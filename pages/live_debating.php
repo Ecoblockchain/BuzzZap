5251cd73ca915fd71a1de3d5310f17452016f320
Caspar Wylie <casparwylie@gmail.com>
2016-02-22 20:54:10 +0000

    live deb dev

--- pages/live_debating.php
+++ pages/live_debating.php
@@ -77,7 +77,7 @@ if($check_valid!="true"){
 		    }
 		}*/
 
-
+		var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 		var peer = new Peer({host: 'www.buzzzap.com', port:9000, path:''});
 
 		peer.on('open', function(id) {
@@ -86,14 +86,43 @@ if($check_valid!="true"){
 
 		$('#start-call').click(function(){
 			var conn = peer.connect(destid);
+			getUserMedia({video: false, audio: true}, function(stream) {
+				var call = peer.call(destid, stream);
+				call.on('stream', function(stream) {
+				    var audio = document.getElementById('audio');
+			   		audio.src = window.URL.createObjectURL(stream);
+			    	audio.onloadedmetadata = function(e){
+			        	console.log('now playing the audio');
+			        	audio.play();
+			   		}
+				});
+			}, function(err) {
+			  console.log('Failed to get local stream' ,err);
+			});
+
+			conn.on('data', function(data) {
+		 		console.log(data);
+			});
 		});
 
 
 		peer.on('connection', function(conn){
 			conn.send('Hello!');
 			console.log("rec: "+conn);
-			conn.on('data', function(data) {
-		 		console.log(data);
+			peer.on('call', function(call) {
+				getUserMedia({video: false, audio: true}, function(stream) {
+			   		call.answer(stream);
+			    	call.on('stream', function(stream) {
+			     		var audio = document.getElementById('audio');
+			   			audio.src = window.URL.createObjectURL(stream);
+			    		audio.onloadedmetadata = function(e){
+			        	console.log('now playing the audio');
+			        	audio.play();
+			   		}
+			    	});
+			  	}, function(err) {
+			    	console.log('Failed to get local stream' ,err);
+			  	});
 			});
 		});
 
@@ -107,4 +136,4 @@ if($check_valid!="true"){
 	});
 </script>
 <div id = "start-call"> Start Call </div>
-<audio controls></audio>
+<audio id = "audio"></audio>
