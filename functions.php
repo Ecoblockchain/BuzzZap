c0daf7572734bfdcb878fe79eda4fb5e26e11a68
Caspar Wylie <casparwylie@gmail.com>
2016-02-26 15:51:27 +0000

    fixes: voting case sensitive fix, and count issue iin error result

--- functions.php
+++ functions.php
@@ -435,7 +435,6 @@ function merge_cus_vote_vals($thread_id){
 
 function vote_debate($vote, $d_id, $cus){
 	global $db;
-	$vote = strtolower($vote);
 	if($cus==false){
 		$assoc_vtypes = array("yes"=>"vote_yes", "no"=>"vote_no", "maybe"=>"vote_maybe", "agree"=>"vote_yes", "disagree"=>"vote_no");
 		$update = $db->prepare("UPDATE debating_threads SET `".$assoc_vtypes[$vote]."` = `".$assoc_vtypes[$vote]."` + 1 WHERE thread_id = ".$db->quote($d_id));
